e3e04d2c4342a6a06ea7b5c9b3aadbf8dca1de43
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-03-28 20:40:56 +0200

    Add files via upload

---
+++ Base.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Base {
+public:
+	Base() { std::cout << "Base constructor\n"; }
+    void show() { std::cout << "Base show"<< '\n'; }
+	~Base() { std::cout << "Base destructor\n"; }
+};
+
+class Derived : public Base 
+{
+public:
+    Derived() {std::cout << "Derived constructor\n"; }
+    void show() {std::cout << "Derived show\n";}
+	~Derived() { std::cout << "Derived destructor\n"; }
+};
+
+int main()
+{
+    {
+        Derived derived;
+        Base* ptr = &derived;
+        ptr->show();
+    }
+
+    return 0;
+}
