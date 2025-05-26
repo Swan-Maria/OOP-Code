1f5ac75dc9919224c3fdece5229a3a8a0430f6fe
Maria <lebedeva.maria@stud.onu.edu.ua>
2025-05-26 12:35:51 +0300

    Add files via upload

--- Student.cpp
+++ Student.cpp
@@ -3,12 +3,14 @@
 #include <list>
 #include <map>
 #include <algorithm>
+#include <numeric>
 
 class Student 
 {
 private:
     using Pair = std::pair<std::string, std::string>; // Имя и фамилия
     using Map = std::map<std::string, size_t>;        // Предмет -> оценка
+    using ElementMap = std::pair<std::string, size_t>;   // Элемент Map
 
     Pair student;
     Map grades;
@@ -35,7 +37,7 @@ public:
         {
             return it->second;
         }
-        std::cerr<<"Subject not found.";
+        return 0;
     }
 
     void printGrades() const 
@@ -46,6 +48,26 @@ public:
             std::cout << "\t" << entry.first << ": " << entry.second << "\n";
         }
     }
+
+    double getAverageGrade() const
+    {
+        if (grades.empty())
+        {
+            return 0.0;
+        }
+
+        double sum = std::accumulate(
+            grades.begin(), grades.end(), 0.0,
+            [](double acc, const ElementMap& pair)
+            {
+                return acc + pair.second;
+            }
+        );
+        
+        return sum / grades.size();
+    }
+
+
 };
 
 class Group
@@ -82,51 +104,92 @@ public:
         {
             it->addGrade(subject, grade);
         }
-        else 
-        {
-           std::cerr<<"Student not found.";
-        }
     }
     
+    double getSubjectAverage(const std::string& subject) const
+    {
+        size_t count = 0;
+        double sum = std::accumulate(
+            students.begin(), students.end(), 0.0,
+            [&](double acc, const Student& student)
+            {
+                size_t grade = student.getGrade(subject);
+                if (grade > 0) 
+                {
+                    ++count;
+                    return acc + grade;
+                }
+                return acc;
+            }
+        );
+        return count ? sum / count : 0.0; 
+    }
+
     void printGroupInfo() const 
     {
-        std::cout << "--- Group Subjects ---\n";
+        std::cout << "------ Group Subjects ------\n";
         for (const auto& subject : subjects) 
         {
-            std::cout << "   -> " << subject << "\n";
-        }
+            std::cout << "   -> " << subject;
+			double average = getSubjectAverage(subject);
+			if (average == 0.0)
+			{
+				std::cout << " (No grades assigned)\n";
+			}
+			else
+			{
+				std::cout << " (Average: " << average << ")\n";
+			}
+		}
 
-        std::cout << "\n--- Student's performance ---\n";
+        std::cout << "\n------ Student's performance ------\n\n";
         for (const auto& student : students) 
         {
             std::cout << "   -> ";
             student.printGrades();
-            //std::cout << "\n";
+			std::cout << "\tAVERAGE GRADE: " << student.getAverageGrade() << "\n";
+            std::cout<<"   ------------------------------\n";
         }
     }
+
+
 };
 
 int main() 
 {
     Group group;
 
-    for(const auto& subject : {"Math Logic", "Operating Systems", "OOP", "DSA"}) 
+    for(const auto& subject : {"Economics","Psychology","Philosophy","Sociology", "Law" })
     {
         group.addSubject(subject);
     }
 
-    Student s1("Maria", "Lebedieva");
+    Student s1("Petr", "Petrov");
 
-    s1.addGrade("OOP", 95);
-    s1.addGrade("DSA", 95);
+    s1.addGrade("Economics", 90);
+	s1.addGrade("Psychology", 85);
+	s1.addGrade("Philosophy", 88);
+	s1.addGrade("Sociology", 92);
+	s1.addGrade("Law", 80);
 
     group.addStudent(s1); 
-    group.addStudent(Student("Daria", "Togan"));
+    group.addStudent(Student("Ivan", "Ivanov"));
+	group.addStudent(Student("Sidor", "Sidorov"));
 
-    group.assignGradeToStudent("Daria Togan", "OOP", 95);
-    group.assignGradeToStudent("Daria Togan", "DSA", 95);
+	group.assignGradeToStudent("Ivan Ivanov", "Economics", 85);
+	group.assignGradeToStudent("Ivan Ivanov", "Psychology", 88);
+	group.assignGradeToStudent("Ivan Ivanov", "Philosophy", 92);
+	group.assignGradeToStudent("Ivan Ivanov", "Sociology", 80);
+	group.assignGradeToStudent("Ivan Ivanov", "Law", 75);
+
+	group.assignGradeToStudent("Sidor Sidorov", "Economics", 70);
+	group.assignGradeToStudent("Sidor Sidorov", "Psychology", 68);
+	group.assignGradeToStudent("Sidor Sidorov", "Philosophy", 72);
+	group.assignGradeToStudent("Sidor Sidorov", "Sociology", 65);
+    group.assignGradeToStudent("Sidor Sidorov", "Law", 75);
 
     group.printGroupInfo();
+
 
     return 0;
 }
